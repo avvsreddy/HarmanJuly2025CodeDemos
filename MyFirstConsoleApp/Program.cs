9d100b559ee4c8d97ae1c2d7504fde9a284fe905
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-09 15:32:54 +0530

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // accept 2 numbers and find the sum, then display
+            // Step 1: declare variables
+            int fno;
+            int sno;
+            int sum;
+
+            // Step 2: input the data
+            Console.WriteLine("Enter first number: ");
+            fno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            sno = int.Parse(Console.ReadLine());
+
+            // Step 3: find the sum
+            sum = fno + sno;
+
+            // Step 4: display the result
+            Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+        }
+
+    }
+}
