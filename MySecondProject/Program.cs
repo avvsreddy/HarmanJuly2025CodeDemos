95b05946ca11f97d8a653ed10030d9d131daac3b
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-09 16:06:30 +0530

    lab1

---
+++ MySecondProject/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MySecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Accept two numbers and find the maximum
+
+
+
+        }
+    }
+}
