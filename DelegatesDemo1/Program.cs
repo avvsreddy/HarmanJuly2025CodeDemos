0726240fa4db78e5bb917dd38064c39adc3c8fe5
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-15 12:09:52 +0530

    delegates demo

---
+++ DelegatesDemo1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace DelegatesDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Direct method invocation
+            //Greeting("Good morning!");
+            // Delegate d = new Delegate();
+            // Step 2: Instatiation and initialization
+            //MyDelegate d = new MyDelegate(Greeting);
+            MyDelegate d = null;
+            Program p = new Program();
+            d += p.Hi; // Subscription
+
+            //d -= Greeting; // Unsubscribe
+
+            d += Greeting; // Sub
+
+            // d += SomeMethod;
+            // Step 3: Invocation
+            //d.Invoke("Good morning!");
+            if (d != null)
+            {
+                d("Good morning again");
+            }
+        }
+
+        public static void Greeting(string text)
+        {
+            Console.WriteLine($"Greeting: {text}");
+        }
+
+        public void Hi(string msg)
+        {
+            Console.WriteLine($"Hi: {msg}");
+        }
+
+        public static string SomeMethod(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+
+    //public class MyDelegate : Delegate
+    //{
+
+    //}
+
+    public delegate void MyDelegate(string msg); // Step:1 - delegate declaration
+    public delegate string MyDelegate1(string str);
+
+
+    public interface IAccount
+    {
+        void Deposit(int amount);
+    }
+
+    public class SavingsAccount : IAccount
+    {
+        public void Deposit(int amount) { }
+    }
+}
---
+++ DelegatesDemo2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+namespace DelegatesDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // client developer 1
+            ProcessManager.ShowProcessList(NoFilter);
+
+            // client 2
+            //FilterDelegate filter = new FilterDelegate(FilterByName);
+            //ProcessManager.ShowProcessList(filter);
+
+            // client 3
+            //ProcessManager.ShowProcessList(FilterBySize);
+        }
+
+        // client 1
+        public static bool NoFilter(Process p)
+        {
+            return true;
+        }
+
+        // client 2
+        public static bool FilterByName(Process p)
+        {
+            return p.ProcessName.StartsWith("S");
+        }
+
+        // client 3
+        public static bool FilterBySize(Process p)
+        {
+            return p.WorkingSet >= 100 * 1024 * 1024;
+        }
+    }
+
+
+
+
+    // backend developer
+
+    public delegate bool FilterDelegate(Process p);
+    public class ProcessManager
+    {
+        //public static void ShowProcessList()
+        //{
+        //    foreach (Process p in Process.GetProcesses())
+        //    {
+        //        Console.WriteLine(p.ProcessName);
+        //    }
+        //}
+
+        public static void ShowProcessList(FilterDelegate filter)
+        {
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (filter(p))
+                    Console.WriteLine(p.ProcessName);
+            }
+        }
+
+        //public static void ShowProcessList(long size)
+        //{
+        //    foreach (Process p in Process.GetProcesses())
+        //    {
+        //        if (p.WorkingSet64 >= size)
+        //            Console.WriteLine(p.ProcessName);
+        //    }
+        //}
+    }
+}
