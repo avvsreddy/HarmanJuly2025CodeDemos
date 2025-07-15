670a51e3ca2ad06522f2e81c7f9bf3069f5c61c4
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-15 09:42:47 +0530

    oo code

---
+++ ExceptionsDemo1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ExceptionsDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // accept 2 ints, find sum, display and repeat
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter first no:");
+                    int fno = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter second no:");
+                    int sno = int.Parse(Console.ReadLine());
+
+                    int sum = fno + sno;
+
+                    Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+                    // close db
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter only numbers");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Please enter small numbers");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                } // catch all block
+                finally
+                {
+                    Console.WriteLine("In finally");
+                }
+
+            }
+        }
+    }
+}
