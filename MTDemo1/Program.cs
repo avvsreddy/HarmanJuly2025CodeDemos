da83ccee4ab66b30f50a07e1c51b1d76be82ad25
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-18 11:01:51 +0530

    MT Demo1

---
+++ MTDemo1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+namespace MTDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application started");
+            Console.WriteLine($"Main Running in {Thread.CurrentThread.ManagedThreadId}");
+            Stopwatch sw = Stopwatch.StartNew();
+
+            M1();
+            M2();
+
+            Console.WriteLine($"Time took to complete: {sw.ElapsedMilliseconds}");
+
+            Console.WriteLine("Runnig with Classic Thread class");
+
+            sw = Stopwatch.StartNew();
+
+            ThreadStart ts1 = new ThreadStart(M1);
+            Thread t1 = new Thread(ts1);
+            t1.Start();
+
+            Thread t2 = new Thread(M2);
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+            Console.WriteLine($"Time took to complete: {sw.ElapsedMilliseconds}");
+
+            Console.WriteLine("Runnig with TPL Task class");
+
+            sw = Stopwatch.StartNew();
+
+            //ThreadStart ts1 = new ThreadStart(M1);
+            Task task1 = new Task(M1);
+            task1.Start();
+
+            //Thread t2 = new Thread(M2);
+            Task task2 = new Task(M2);
+            task2.Start();
+
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine($"Time took to complete: {sw.ElapsedMilliseconds}");
+
+
+
+            Console.WriteLine("Runnig with TPL Parallel class");
+
+            sw = Stopwatch.StartNew();
+
+
+            Parallel.Invoke(M1, M2);
+
+            Console.WriteLine($"Time took to complete: {sw.ElapsedMilliseconds}");
+
+
+
+            Console.WriteLine("end of application");
+        }
+
+        static void M1()
+        {
+            Console.WriteLine($"M1 Running in {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        static void M2()
+        {
+            Console.WriteLine($"M2 Running in {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
