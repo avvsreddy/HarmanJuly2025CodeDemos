9b895c5a64ff95dc4e07bf71bc7f7b52ac049162
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-22 14:45:35 +0530

    ef demo

--- EFDemo1/Program.cs
+++ EFDemo1/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace EFDemo1
+﻿using EFDemo1.Data;
+using EFDemo1.Entities;
+
+namespace EFDemo1
 {
     internal class Program
     {
@@ -14,8 +17,66 @@
             // Step 4: Configure DB and Map Classes with Tables - done
             // STep 5: Data Migration - each time when dbcontext or entity classess changes -> PMC/CLI
 
+            //SaveProduct();
+            //GetAll();
 
+            //Delete();
+            // Update
 
+            //Update();
+
+        }
+
+        private static void Update()
+        {
+            ProductsDbContext db = new ProductsDbContext();
+            // get the object to edit
+            var productToEdit = db.Products.Find(2);
+            productToEdit.Price += 1000;
+            db.SaveChanges();
+        }
+
+        private static void Delete()
+        {
+            // Delete
+            ProductsDbContext db = new ProductsDbContext();
+            // Get the object to delete
+            var productToDelete = (from p in db.Products
+                                   where p.ProductId == 1
+                                   select p).FirstOrDefault();
+            db.Products.Remove(productToDelete);
+            db.SaveChanges();
+        }
+
+        private static void GetAll()
+        {
+            //User Story: I as a user, i want to get all products from database, so that i can display
+
+            ProductsDbContext db = new ProductsDbContext();
+            // Linq to Entities
+            var allProducts = from p in db.Products
+                              select p;
+
+            // display
+            foreach (var item in allProducts)
+            {
+                Console.WriteLine($"{item.ProductId}\t{item.ProductName}\t{item.Price}");
+            }
+        }
+
+        private static void SaveProduct()
+        {
+            // Step 6: Save product details into database
+            Product p = new Product();
+            Console.Write("Enter Product Name: ");
+            p.ProductName = Console.ReadLine();
+            Console.Write("Enter Product Price: ");
+            p.Price = int.Parse(Console.ReadLine());
+
+            ProductsDbContext db = new ProductsDbContext();
+            db.Products.Add(p);
+            db.SaveChanges();
+            Console.WriteLine("Product saved.....");
         }
     }
 }
