b3a8c97dde7c2088841a9b0a2851e724f8cb1bd2
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-28 11:01:35 +0530

    api 2

---
+++ HarmanCoolProductsService/Models/Data/HarmansCoolProductsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HarmanCoolProductsService.Models.Data
+{
+    public class HarmansCoolProductsDbContext : DbContext
+    {
+        // configure DB
+
+        // Map Entities with Tables
+    }
+}
---
+++ HarmanCoolProductsService/Models/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HarmanCoolProductsService.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [MinLength(3)]
+        public string Name { get; set; }
+        [Range(1, 9999999)]
+        public int Price { get; set; }
+        [MaxLength(50)]
+        public string Catagory { get; set; }
+        [MaxLength(50)]
+        public string Brand { get; set; }
+        [MaxLength(50)]
+        public string Country { get; set; }
+        public bool Instock { get; set; }
+    }
+}
