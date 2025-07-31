ceb02eb80a6ee3c029be27904c87fd44279f5c1e
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-07-31 10:16:00 +0530

    auth api

---
+++ SecuredAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecuredAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("anonymous")]
+        public IActionResult Secured1Endpoint()
+        {
+            return Ok("This is for anonymous users users only");
+        }
+
+        [HttpGet("user")]
+        public IActionResult Secured2Endpoint()
+        {
+            return Ok("This is for registered users only");
+        }
+
+        [HttpGet("admin")]
+        public IActionResult Secured3Endpoint()
+        {
+            return Ok("This is for admin users only");
+        }
+    }
+}
---
+++ SecuredAPI/Program.cs
@@ -0,0 +1,42 @@
+
+namespace SecuredAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+
+            // Add Swagger services
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(); // optional: you can configure endpoint here
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
