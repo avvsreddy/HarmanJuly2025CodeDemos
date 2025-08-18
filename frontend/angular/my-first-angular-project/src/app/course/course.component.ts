5c1cff606f3880ad2fc75e9711460506270a3aa0
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-08-18 12:47:01 +0530

    validation

--- frontend/angular/my-first-angular-project/src/app/course/course.component.ts
+++ frontend/angular/my-first-angular-project/src/app/course/course.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CourseRegistration } from '../../models/course-registration';
 import { FormsModule } from '@angular/forms';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-course',
-  imports: [FormsModule,NgFor],
+  imports: [FormsModule,NgFor,NgIf],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
@@ -28,7 +28,8 @@ registrationDate: new Date()
 courses:string[] = ['Angular', 'React', 'Vue', '.Net','Java'];
 
 onSubmit(){
-  console.log(this.course);
+  //console.log(formCtrl);
+  //console.log(this.course);
   // Here you can add logic to handle the form submission, like sending data to a server
 }
 
